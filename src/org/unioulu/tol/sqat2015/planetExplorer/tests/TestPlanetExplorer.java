729317596a45aced41e60145b13cdade9658844b
somename <someemail>
2015-10-22 12:32:15 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -19,4 +19,10 @@ public class TestPlanetExplorer {
 		PlanetExplorer explorer = new PlanetExplorer(100,100,"");
 		assertEquals("0,0,E", explorer.executeCommand("r"));
 	}
+	
+	@Test
+	public void testTurningRRNewFacingS() {
+		PlanetExplorer explorer = new PlanetExplorer(100,100,"");
+		assertEquals("0,0,S", explorer.executeCommand("rr"));
+	}
 }
