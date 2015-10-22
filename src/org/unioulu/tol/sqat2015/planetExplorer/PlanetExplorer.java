b84a739385c997edd597c36082db81fc37b8f550
somename <someemail>
2015-10-22 13:23:18 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -30,8 +30,7 @@ public class PlanetExplorer {
 		
 		for (int i = 0; i < command.length(); i++) {
 			cmd = command.substring(i, i+1);
-			System.out.println(cmd);
-			System.out.println(facing);
+
 			if (cmd == "r") {
 				if (facing == "N")
 					facing = "E";
@@ -48,7 +47,7 @@ public class PlanetExplorer {
 					}
 				}
 			}
-			System.out.println(facing);
+
 			if (cmd == "l") {
 				if (facing == "N")
 					facing = "W";
